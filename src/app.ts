a3445e84f8fe3e9b99e08edcd354986d1486941b
pengchujin <pengchujin@hotmail.com>
2019-06-04 17:19:11 +0800

    添加上传文件

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { createConnection } from 'typeorm';
 import * as express from 'express';
-import * as cors from "cors";
+import * as cors from 'cors';
+import * as multer from 'multer';
 import * as requestIp from 'request-ip';
 import * as process from 'process';
+import * as fs from 'fs';
 import * as bodyParser from 'body-parser';
 const xmlparser = require('express-xml-bodyparser');
 import { altairExpress } from 'altair-express-middleware';
@@ -11,6 +13,8 @@ global.Promise = require('bluebird');
 import * as config from '../config';
 import { schema } from './schema';
 const logger = require('./logger');
+
+const upload = multer({ dest: config.IMAGEFILE });
 
 (async () => {
   // createConnection method will automatically read connection options from your ormconfig file
@@ -33,6 +37,24 @@ const logger = require('./logger');
       };
     })
   );
+
+  app.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+      console.log(req['file']);
+      fs.rename(req['file'].path, 'uploads/' + req['file'].originalname, function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('上传成功!');
+    });
+    } catch (err) {
+        res.sendStatus(400);
+    }
+    res.send({
+      status: true,
+      file: req['file'].originalname
+    });
+})
 
   const path = '/graphql';
 
