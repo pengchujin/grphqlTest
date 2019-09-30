5499c2a958ea2ed6d76350411c3781718f27a065
pengchujin <2093959501@qq.com>
2019-09-30 10:17:06 +0800

    reset entity

--- src/entity/Collection.ts
+++ src/entity/Collection.ts
@@ -6,15 +6,7 @@ export class Collection {
   @Column({nullable: true})
   name: string;
   @Column({nullable: true})
-  uuid: string;
-  @Column({nullable: true})
-  productId: number;
-  @Column({nullable: true})
-  tagId: number;
-  @Column('text', {array: true, nullable: true})
-  elementIds: number[];
-  @Column({nullable: true})
-  combinationId: number;
+  detailInfo: string;
   @ManyToOne((type) => Vip, (vip) => vip.collections)
   vip: Vip;
 }
